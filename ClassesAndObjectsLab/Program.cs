647db92082afe49ef1d397164824de1885ce6877
Seth Johnson <sethjdesigns@gmail.com>
2015-03-26 21:12:30 -0400

    Cleaned up code a little bit

--- ClassesAndObjectsLab/Program.cs
+++ ClassesAndObjectsLab/Program.cs
@@ -5,33 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassesAndObjectsLab
-{
-
-    //I am trying to create an instance of student, but I keep getting an error that
-    //says "seth" does not exist in the current context. I am trying to create a new
-    //instance of Student and passing it in the inital name value of seth.
-    //WHY WONT IT WORK GODDAMMIT!
-    
+{   
     class Program
     {
 
         static void Main()
         {
 
-            Student newStudent = new Student("seth");
-            Console.WriteLine(newStudent.ToString());
+            Student newStudent = new Student("Seth");
 
-            newStudent.FinishedProgram();
-            Console.WriteLine(newStudent.ToString());
-
-            newStudent.HelpedStudent();
             Console.WriteLine(newStudent.ToString());
 
             Console.ReadLine();
         }
     }
-
-
-
-    
 }
